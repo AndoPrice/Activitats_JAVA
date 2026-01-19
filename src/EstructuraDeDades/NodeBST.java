1b7531f0992d3c0a2fd86fff1fc1fac0eacd225d
AndoPrice <andolucprice@gmail.com>
2026-01-19 11:46:02 +0100

    Properties

--- src/EstructuraDeDades/NodeBST.java
+++ src/EstructuraDeDades/NodeBST.java
@@ -1,5 +1,7 @@
 package EstructuraDeDades;
 
+import processing.core.PApplet;
+
 public class NodeBST {
     int valor;
     NodeBST esq, dret;
@@ -60,6 +62,26 @@ public class NodeBST {
             this.esq.revers();
         }
     }
+
+    public void display(PApplet p5, float x, float y, float r, int level){
+
+        float dx = (p5.width/3)/(level+2);
+        p5.pushStyle();
+
+        if(this.esq!=null){
+            p5.line(x, y, x - dx, y + 2*r);
+        }
+
+        if(this.dret!=null){
+            p5.line(x, y, x + dx, y + 2*r);
+        }
+
+        p5.ellipse(x, y, r,r);
+        p5.fill(0); p5.textSize(18); p5.textAlign(p5.CENTER);
+        p5.text(this.valor, x, y + r/6);
+        p5.popStyle();
+    }
+
 
 
 
